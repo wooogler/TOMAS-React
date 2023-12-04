e9ecb5cb43a7fec0e5c7f6c1b919e458811fbf5e
Sangwook Lee <leesang627@gmail.com>
2023-12-04 10:40:23 +0900

    Update file paths and URLs

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/TOMAS-React/sw.js', { scope: '/TOMAS-React/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
